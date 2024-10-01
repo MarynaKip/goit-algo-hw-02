f416cf0f08a66be3ad5330c9b2efdb28c8efca38
Maryna Sibova <maryna.i.kipenko@gmail.com>
2024-10-01 00:22:41 -0400

    task 1 is updated

--- main.py
+++ main.py
@@ -14,20 +14,24 @@ class Request:
 
 # Функція для автоматичної генерації заявок
 def generate_request(queue, request_id_counter):
-    while True:
+    while not stop_event.is_set():
+        if stop_event.is_set():
+            break
         # Створюємо нову заявку з унікальним ідентифікатором
         description = f"Опис заявки {request_id_counter}"
         new_request = Request(request_id_counter, description)
         queue.put(new_request)
         print(f"[Нова заявка] {new_request}")
-        ç += 1
+        request_id_counter += 1
         
         # Затримка між генерацією нових заявок (імітація часу)
         time.sleep(random.uniform(1, 3))
 
 # Функція для обробки заявок
 def process_request(queue):
-    while True:
+    while not stop_event.is_set() or not queue.empty():
+        if stop_event.is_set():
+            break
         # Чекаємо поки з'явиться заявка для обробки
         request = queue.get()
         print(f"[Обробка] {request}")
@@ -40,19 +44,26 @@ def process_request(queue):
         print(f"[Завершено] {request}")
 
 if __name__ == "__main__":
-    # Створюємо чергу для зберігання заявок
-    request_queue = queue.Queue()
-    request_id_counter = 1
+    try:
+        # Створюємо чергу для зберігання заявок
+        request_queue = queue.Queue()
+        stop_event = threading.Event()  # Подія для зупинки потоків
+        request_id_counter = 1
 
-    # Запускаємо поток для генерації заявок
-    generator_thread = threading.Thread(target=generate_request, args=(request_queue, request_id_counter))
-    generator_thread.daemon = True
-    generator_thread.start()
+        # Запускаємо поток для генерації заявок
+        generator_thread = threading.Thread(target=generate_request, args=(request_queue, request_id_counter))
+        generator_thread.daemon = True
+        generator_thread.start()
 
-    # Запускаємо поток для обробки заявок
-    processor_thread = threading.Thread(target=process_request, args=(request_queue,))
-    processor_thread.daemon = True
-    processor_thread.start()
+        # Запускаємо поток для обробки заявок
+        processor_thread = threading.Thread(target=process_request, args=(request_queue,))
+        processor_thread.daemon = True
+        processor_thread.start()
 
-    # Чекаємо завершення обробки всіх заявок (можна використовувати для завершення програми)
-    request_queue.join()
+        # Чекаємо завершення обробки всіх заявок (можна використовувати для завершення програми)
+        request_queue.join()
+    except KeyboardInterrupt:
+        # Надсилаємо сигнал для зупинки потоків
+        stop_event.set()
+        print("Завершення програми...")
+        request_queue.join()
